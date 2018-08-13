5b7a6fdfa96a1196a4cf23507c1ba81bd23ffc3b
João Teodoro <joaotdn@gmail.com>
2018-08-13 02:24:01 -0300

    Add Husky

---
+++ src/main.js
@@ -0,0 +1,8 @@
+a = 3;
+const b = 9
+
+const b=6
+
+console.log("test")
+
+const c = "slkdfhklfhsldkfhslkfhsuosiuhfsoifhreinhfsoirnfhsioeurhnfisounfhoierhfnisoernhfosiunhfosieurnhfsoirefnoseinhfouiren"
